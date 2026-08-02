397ecb938362e25b764fb53b44ebc0cf4ef2cef3
Pravardhan Tripathi <pravardhantripathi@gmail.com>
2026-08-03 02:27:55 +0530

    Update API URL to Render backend

--- react-frontend/src/App.jsx
+++ react-frontend/src/App.jsx
@@ -94,7 +94,7 @@ function App() {
     setResults(null);
 
     try {
-      const response = await fetch('http://127.0.0.1:8000/predict', {
+      const response = await fetch('https://prophecyai.onrender.com/predict', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({
